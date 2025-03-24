3cad59111aa134de7b07f04599d0986338bf7fb0
CadarIustin <iustin.cadar@student.nhlstenden.com>
2025-03-24 23:35:28 +0100

    Add package-info.java to organize test package structure

---
+++ test/jabberpoint/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains test classes for the Jabberpoint application.
+ * The tests are written using JUnit 5 and cover various functionality of the application.
+ */
+package jabberpoint;

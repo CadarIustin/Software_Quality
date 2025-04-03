c7ed8f2b8cec592b830a1ef8c3fb59bad7b77343
beligeanupatrick <111996973+beligeanupatrick@users.noreply.github.com>
2025-04-04 01:16:36 +0200

    Add presentation export functionality
    This commit adds:
    - PresentationExporter interface for the Strategy pattern
    - HTMLExporter implementation for exporting to web format
    - TextExporter implementation for plain text exports
    - Enhanced MenuController with export options and slide editor integration

--- src/jabberpoint/controller/MenuController.java
+++ src/jabberpoint/controller/MenuController.java
@@ -16,10 +16,14 @@ import jabberpoint.model.Presentation;
 import jabberpoint.model.Style;
 import jabberpoint.util.DefaultTheme;
 import jabberpoint.util.DarkTheme;
+import jabberpoint.util.HTMLExporter;
+import jabberpoint.util.PresentationExporter;
+import jabberpoint.util.TextExporter;
 import jabberpoint.util.ThemeStrategy;
 import jabberpoint.util.XMLLoader;
 import jabberpoint.util.XMLSaver;
 import jabberpoint.view.AboutBox;
+import jabberpoint.view.SlideEditorFrame;
 
 public class MenuController extends MenuBar {
     private static final long serialVersionUID = 227L;
@@ -37,10 +41,16 @@ public class MenuController extends MenuBar {
     private static final String PAGENR = "Page number?";
     private static final String PREV = "Prev";
     private static final String SAVE = "Save";
+    private static final String EXPORT = "Export";
+    private static final String EXPORT_HTML = "Export to HTML";
+    private static final String EXPORT_TEXT = "Export to Text";
+    private static final String EDIT = "Edit";
+    private static final String EDIT_PRESENTATION = "Edit Presentation";
     private static final String VIEW = "View";
     private static final String THEMES = "Themes";
     private static final String DEFAULT_THEME = "Default Theme";
     private static final String DARK_THEME = "Dark Theme";
+    private static final String CYCLE_TRANSITION = "Cycle Transition";
     
     private static final String TESTFILE = "test.xml";
     private static final String SAVEFILE = "presentation.xml";
@@ -48,6 +58,7 @@ public class MenuController extends MenuBar {
     private static final String IOEX = "IO Exception: ";
     private static final String LOADERR = "Load Error";
     private static final String SAVEERR = "Save Error";
+    private static final String EXPORTERR = "Export Error";
 
     public MenuController(Frame frame, Presentation pres) {
         parent = frame;
@@ -77,6 +88,25 @@ public class MenuController extends MenuBar {
             }
         });
         
+        // Export submenu
+        Menu exportMenu = new Menu(EXPORT);
+        
+        exportMenu.add(menuItem = new MenuItem(EXPORT_HTML));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                exportPresentation(new HTMLExporter());
+            }
+        });
+        
+        exportMenu.add(menuItem = new MenuItem(EXPORT_TEXT));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                exportPresentation(new TextExporter());
+            }
+        });
+        
+        fileMenu.add(exportMenu);
+        
         fileMenu.addSeparator();
         
         fileMenu.add(menuItem = mkMenuItem(EXIT));
@@ -87,6 +117,17 @@ public class MenuController extends MenuBar {
         });
         
         add(fileMenu);
+        
+        // Edit menu
+        Menu editMenu = new Menu(EDIT);
+        editMenu.add(menuItem = mkMenuItem(EDIT_PRESENTATION));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                openSlideEditor();
+            }
+        });
+        
+        add(editMenu);
         
         Menu viewMenu = new Menu(VIEW);
         viewMenu.add(menuItem = mkMenuItem(NEXT));
@@ -107,6 +148,13 @@ public class MenuController extends MenuBar {
         menuItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 gotoSlide();
+            }
+        });
+        
+        viewMenu.add(menuItem = new MenuItem(CYCLE_TRANSITION));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cycleTransition();
             }
         });
         
@@ -182,6 +230,21 @@ public class MenuController extends MenuBar {
         }
     }
     
+    private void exportPresentation(PresentationExporter exporter) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new java.io.File("."));
+        fileChooser.setDialogTitle("Export as " + exporter.getDescription());
+        int returnVal = fileChooser.showSaveDialog(parent);
+        
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                exporter.exportPresentation(presentation, fileChooser.getSelectedFile().getPath());
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(parent, IOEX + ex, EXPORTERR, JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+    
     private void newPresentation() {
         presentation.clear();
         presentation.setTitle("New Presentation");
@@ -192,5 +255,22 @@ public class MenuController extends MenuBar {
         String pageNumberStr = JOptionPane.showInputDialog(PAGENR);
         int pageNumber = Integer.parseInt(pageNumberStr);
         presentation.setSlideNumber(pageNumber - 1);
+    }
+    
+    private void openSlideEditor() {
+        new SlideEditorFrame(presentation);
+    }
+    
+    private void cycleTransition() {
+        // Find the SlideViewerComponent to cycle its transition
+        if (parent instanceof jabberpoint.view.SlideViewerFrame) {
+            jabberpoint.view.SlideViewerFrame frame = (jabberpoint.view.SlideViewerFrame) parent;
+            frame.getSlideViewerComponent().cycleTransitionType();
+            String transitionName = frame.getSlideViewerComponent().getCurrentTransitionName();
+            JOptionPane.showMessageDialog(parent, 
+                "Transition changed to: " + transitionName, 
+                "Transition Type", 
+                JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
